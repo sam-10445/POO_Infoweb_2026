c30636c8c2bf8f06fd8200289f1df4afaa1d1a8a
sam-10445 <smirandanovaes@gmail.com>
2026-05-10 12:22:44 +0000

    correção do github e dos arquivos

--- aula11/Tarefa04-Listas/Playlist_e_musicas/ex2_Playlist_e_musicas.py
+++ aula11/Tarefa04-Listas/Playlist_e_musicas/ex2_Playlist_e_musicas.py
@@ -1,184 +1,260 @@
-class Playlist:
-    def __init__(self, id, nome, descricao):
+#CLASSES
+class PlayList:
+    #init
+    def __init__(self, id, nome, desc):
         self.set_id(id)
         self.set_nome(nome)
-        self.set_descricao(descricao)
-
-    #gets
-    def get_id(self): return self.__id
-    def get_nome(self): return self.__nome
-    def get_descricao(self): return self.__descricao
-
-    #sets
+        self.set_desc(desc)
+    #set
     def set_id(self, id):
         if id < 0: raise ValueError()
         self.__id = id
-
     def set_nome(self, nome):
-        if nome == "": raise ValueError()
+        if len(nome) == 0: raise ValueError()
         self.__nome = nome
-
-    def set_descricao(self, descricao):
-        if descricao == "": raise ValueError()
-        self.__descricao = descricao
-
+    def set_desc(self, desc):
+        if desc == '': raise ValueError()
+        self.__desc = desc
+    #gets
+    def get_id(self): return self.__id
+    def get_nome(self): return self.__nome
+    def get_desc(self): return self.__desc
     #ToString
     def __str__(self):
-        return f"ID: {self.__id} | Nome: {self.__nome} | Descrição: {self.__descricao}"
-
+        return f"ID: {self.__id} | Nome: {self.__nome} | Descrição: {self.__desc}"
+    
 class Musica:
+    #init
     def __init__(self, id, titulo, artista, album):
         self.set_id(id)
         self.set_titulo(titulo)
         self.set_artista(artista)
         self.set_album(album)
-
-    #gets
+    #set
+    def set_id(self, id):
+        if id < 0: raise ValueError()
+        self.__id = id
+    def set_titulo(self, titulo):
+        if titulo == '': raise ValueError()
+        self.__titulo = titulo
+    def set_artista(self, artista):
+        if artista == '': raise ValueError()
+        self.__artista = artista
+    def set_album(self, album):
+        if album == '': raise ValueError()
+        self.__album = album
+    #get
     def get_id(self): return self.__id
     def get_titulo(self): return self.__titulo
     def get_artista(self): return self.__artista
     def get_album(self): return self.__album
-
-    #sets
+    #ToString
+    def __str__(self):
+        return f"ID: {self.__id} | Título: {self.__titulo} | Artista: {self.__artista} | Album: {self.__album}"
+    
+class PlayListItem:
+    #init
+    def __init__(self, id, idPlayList, idMusica, seque):
+        self.set_id(id)
+        self.set_idPlayList(idPlayList)
+        self.set_idMusica(idMusica)
+        self.set_seque(seque)
+    #set
     def set_id(self, id):
         if id < 0: raise ValueError()
         self.__id = id
-
-    def set_titulo(self, titulo):
-        if titulo == "": raise ValueError()
-        self.__titulo = titulo
-
-    def set_artista(self, artista):
-        if artista == "":
-            raise ValueError()
-
-        self.__artista = artista
-
-    def set_album(self, album):
-        if album == "": raise ValueError()
-        self.__album = album
-
-    #ToString
-    def __str__(self):
-        return f"ID: {self.__id} | Título: {self.__titulo} | Artista: {self.__artista} | Álbum: {self.__album}"
-
-class PlaylistItem:
-    def __init__(self, id, id_playlist, id_musica, sequencia):
-        self.set_id(id)
-        self.set_id_playlist(id_playlist)
-        self.set_id_musica(id_musica)
-        self.set_sequencia(sequencia)
-
+    def set_idPlayList(self, idPlayList):
+        if idPlayList < 0: raise ValueError()
+        self.__idPlayList = idPlayList
+    def set_idMusica(self, idMusica):
+        if idMusica < 0: raise ValueError()
+        self.__idMusica = idMusica
+    def set_seque(self, seque):
+        if seque < 0: raise ValueError()
+        self.__seque = seque
     #gets
     def get_id(self): return self.__id
-    def get_id_playlist(self): return self.__id_playlist
-    def get_id_musica(self): return self.__id_musica
-    def get_sequencia(self): return self.__sequencia
-
-    #sets
-    def set_id(self, id):
-        if id < 0: raise ValueError()
-        self.__id = id
-
-    def set_id_playlist(self, id_playlist):
-        if id_playlist < 0: raise ValueError()
-        self.__id_playlist = id_playlist
-
-    def set_id_musica(self, id_musica):
-        if id_musica < 0: raise ValueError()
-        self.__id_musica = id_musica
-
-    def set_sequencia(self, sequencia):
-        if sequencia < 0: raise ValueError()
-        self.__sequencia = sequencia
-
+    def get_idPlayList(self): return self.__idPlayList
+    def get_idMusica(self): return self.__idMusica
+    def get_seque(self): return self.__seque
     #ToString
     def __str__(self):
-        return f"ID: {self.__id} | Playlist: {self.__id_playlist} | Música: {self.__id_musica} | Sequência: {self.__sequencia}"
-
-#INTERFACE COM O USUÁRIO
+        return f"ID: {self.__id} | ID da PlayList: {self.__idPlayList} | ID da Música: {self.__idMusica} | Sequência: {self.__seque}"
+    
+#CLASS UI
 class UI:
-    #criar as listas
+    #listas
     playlists = []
     musicas = []
     itens = []
 
-    def inserir_playlist(self):
-        id = int(input("ID da playlist: "))
-        nome = input("Nome da playlist: ")
-        descricao = input("Descrição: ")
+    #main (op, wheli, op = UI.menu())
+    @staticmethod
+    def main():
+        op = -1
+        while op != 0:
+            op = UI.menu()
+            if op == 1: UI.inserir_playlist()
+            if op == 2: UI.listar_playlists()
+            if op == 3: UI.atualizar_playlist()
+            if op == 4: UI.excluir_playlist()
+            if op == 5: UI.inserir_musica()
+            if op == 6: UI.listar_musicas()
+            if op == 7: UI.atualizar_musica()
+            if op == 8: UI.excluir_musica()
+            if op == 9: UI.listar_musica_playlist()
+            if op == 10: UI.transferir()
+            if op == 11: UI.inserir_item()
+            if op == 12: UI.listar_itens()
+        print('Programa Encerrado.')
+    #menu(print..., return int(input(...)))
+    @staticmethod
+    def menu():
+        print('1 - inserir playlist')
+        print('2 - listar playlists')
+        print('3 - atualizar playlist')
+        print('4 - excluir playlist')
+        print('5 - inserir musica')
+        print('6 - listar musicas')
+        print('7 - atualizar musica')
+        print('8 - excluir musica')
+        print('9 - listar musicas de uma playlist')
+        print('10 - transferir musica')
+        print('11 - Adicionar Item')
+        print('12 - Listar Itens')
+        print('0 - Sair')
+        return int(input('Escolha: '))
+    
+    #AS AÇÕES
+    @classmethod
+    def inserir_playlist(cls):
+        id = int(input('ID da PlayList: '))
+        nome = input('Nome: ')
+        desc = input('Descrição: ')
 
-        playlist = Playlist(id, nome, descricao)
-        self.playlists.append(playlist)
+        x = PlayList(id, nome, desc) #coloco no Objeto 
+        cls.playlists.append(x) #coloco na lista
+        print('Adicionada!')
 
-        print("Playlist cadastrada!\n")
-
-    def listar_playlists(self):
-        if len(self.playlists) == 0:
-            print("Nenhuma playlist cadastrada.\n")
+    @classmethod
+    def listar_playlists(cls):
+        if len(cls.playlists) == 0: #vê se a lista não tá vazia
+            print('Não há playlists cadastradas.')
         else:
-            for playlist in self.playlists:
-                print(playlist)
+            for x in cls.playlists: #vare a lista da playlist
+                print(x) #ir printando as playlists
 
-    def inserir_musica(self):
-        id = int(input("ID da música: "))
-        titulo = input("Título: ")
-        artista = input("Artista: ")
-        album = input("Álbum: ")
+    @classmethod
+    def atualizar_playlist(cls):
+        id = int(input('ID da PlayList: ')) #pergunta qual a playlist
 
-        musica = Musica(id, titulo, artista, album)
-        self.musicas.append(musica)
+        for x in cls.playlists: #vare a lista de playlists
+            if x.get_id() == id: #acha a playlist
+                novo_nome = input('Novo Nome: ') #perguntar 
+                nova_desc = input('Nova Descrição: ')
 
-        print("Música cadastrada!\n")
+                x.set_nome(novo_nome) #substituir 
+                x.set_desc(nova_desc)
+                print('Atualizada!')
 
+    @classmethod
+    def excluir_playlist(cls):
+        id = int(input('ID da Playlist: '))
 
-    def listar_musicas(self):
-        if len(self.musicas) == 0:
-            print("Nenhuma música cadastrada.\n")
+        for x in cls.playlists[:]:
+            if x.get_id() == id:
+                cls.playlists.remove(x)
+
+                #remover os itens da playlist
+                for item in cls.itens[:]:
+                    if item.get_idPlayList() == id:
+                        cls.itens.remove(item)
+                print('Removida!')
+
+    @classmethod
+    def inserir_musica(cls):
+        id = int(input('ID da Música: '))
+        titulo = input('Título: ')
+        artista = input('Artista: ')
+        album = input('Album: ')
+
+        x = Musica(id, titulo, artista, album)
+        cls.musicas.append(x)
+        print('Adicionada!')
+
+    @classmethod
+    def listar_musicas(cls):
+        if len(cls.musicas) == 0: 
+            print('Não há músicas cadastradas.')
         else:
-            for musica in self.musicas:
-                print(musica)
-            print()
+            for x in cls.musicas:
+                print(x)
 
-    def inserir_item(self):
-        id = int(input("ID do item: "))
-        id_playlist = int(input("ID da playlist: "))
-        id_musica = int(input("ID da música: "))
-        sequencia = int(input("Sequência da música: "))
+    @classmethod
+    def atualizar_musica(cls):
+        id = int(input('ID da Música: '))
 
-        item = PlaylistItem(id, id_playlist, id_musica, sequencia)
-        self.itens.append(item)
+        for x in cls.musicas:
+            if x.get_id() == id:
+                novo_titulo = input('Novo Título: ')
+                novo_artista = input('Novo Artista: ')
+                novo_album = input('Novo Álbum: ')
 
-        print("Item cadastrado!\n")
+                x.set_titulo(novo_titulo)
+                x.set_artista(novo_artista)
+                x.set_album(novo_album)
+                print('Atualizada!')
 
-    def listar_itens(self):
-        if len(self.itens) == 0:
-            print("Nenhum item cadastrado.\n")
+    @classmethod
+    def excluir_musica(cls):
+        id = int(input('ID da Música: '))
+
+        for x in cls.musicas:
+            if x.get_id() == id:
+                cls.musicas.remove(x)
+
+                #remover os itens da música
+                for item in cls.itens:
+                    if item.get_idMusica() == id:
+                        cls.itens.remove(item)
+                print('Removida!')
+
+    @classmethod
+    def listar_musica_playlist(cls):
+        id = int(input('ID da Playlist: '))
+
+        for x in cls.musicas:
+            if x.get_idPlaylist() == id:
+                print(x)
+
+    @classmethod
+    def transferir(cls):
+        idMusica = int(input('ID da Música: '))
+        novo_idPlaylist = int(input('Novo ID da Playlist: '))
+
+        for item in cls.itens:
+            if item.get_idMusica() == idMusica:
+                item.set_idPlayList(novo_idPlaylist)
+                print('Transferida!')
+
+    @classmethod
+    def inserir_item(cls):
+        id = int(input('ID do Item: '))
+        idPlayList = int(input('ID da Playlist: '))
+        idMusica = int(input('ID da Música: '))
+        seque = int(input('Sequência: '))
+
+        x = PlayListItem(id, idPlayList, idMusica, seque)
+        cls.itens.append(x)
+
+        print('Item adicionado!')
+
+    @classmethod
+    def listar_itens(cls):
+        if len(cls.itens) == 0:
+            print('Não há itens.')
         else:
-            for item in self.itens:
-                print(item)
+            for x in cls.itens:
+                print(x)
 
-    #menu
-    def main(self):
-        opcao = 100000
-
-        while opcao != 0:
-            print("1 - Inserir playlist")
-            print("2 - Listar playlists")
-            print("3 - Inserir música")
-            print("4 - Listar músicas")
-            print("5 - Inserir item da playlist")
-            print("6 - Listar itens")
-            print("0 - Sair")
-            opcao = int(input("Informe uma opção: "))
-
-            if opcao == 1: self.inserir_playlist()
-            if opcao == 2: self.listar_playlists()
-            if opcao == 3: self.inserir_musica()
-            if opcao == 4: self.listar_musicas()
-            if opcao == 5: self.inserir_item()
-            if opcao == 6: self.listar_itens()
-            if opcao == 0: print("Tchau! :P")
-
-ui = UI()
-ui.main()
+UI.main()
--- aula11/Tarefa04-Listas/Times_e_jogadores/ex1_Times_e_jogadores.py
+++ aula11/Tarefa04-Listas/Times_e_jogadores/ex1_Times_e_jogadores.py
@@ -1,4 +1,6 @@
+#CLASS
 class Time:
+    #init
     def __init__(self, id, nome, estado):
         self.set_id(id)
         self.set_nome(nome)
@@ -6,15 +8,15 @@ class Time:
 
     #sets
     def set_id(self, id):
-        if id < 0: ValueError() #id negativo
+        if id < 0: raise ValueError()
         self.__id = id
 
     def set_nome(self, nome):
-        if nome == "": ValueError()
+        if len(nome) == 0: raise ValueError()
         self.__nome = nome
 
     def set_estado(self, estado):
-        if estado == "": ValueError()
+        if len(estado) == 0: raise ValueError()
         self.__estado = estado
 
     #gets
@@ -26,202 +28,196 @@ class Time:
     def __str__(self):
         return f"ID: {self.__id} | Nome: {self.__nome} | Estado: {self.__estado}"
 
+
 class Jogador:
-    def __init__(self, id, id_time, nome, camisa):
+    #init
+    def __init__(self, id, idTime, nome, camisa):
         self.set_id(id)
-        self.set_id_time(id_time)
+        self.set_idTime(idTime)
         self.set_nome(nome)
         self.set_camisa(camisa)
 
     #sets
     def set_id(self, id):
-        if id < 0: ValueError()
+        if id < 0: raise ValueError()
         self.__id = id
 
-    def set_id_time(self, id_time):
-        if id_time < 0: ValueError()
-        self.__id_time = id_time
+    def set_idTime(self, idTime):
+        if idTime < 0: raise ValueError()
+        self.__idTime = idTime
 
     def set_nome(self, nome):
-        if nome == "": ValueError()
+        if len(nome) == 0: raise ValueError()
         self.__nome = nome
 
     def set_camisa(self, camisa):
-        if camisa < 0: ValueError()
+        if camisa < 0: raise ValueError()
         self.__camisa = camisa
 
     #gets
     def get_id(self): return self.__id
-
-    def get_id_time(self): return self.__id_time
-
+    def get_idTime(self): return self.__idTime
     def get_nome(self): return self.__nome
-
     def get_camisa(self): return self.__camisa
 
     #ToString
     def __str__(self):
-        return f"ID: {self.__id} | Nome: {self.__nome} | Camisa: {self.__camisa} | Time: {self.__id_time}"
+        return f"ID: {self.__id} | ID do Time: {self.__idTime} | Nome: {self.__nome} | Camisa: {self.__camisa}"
 
-#INTERFACE DO USUÁRIO
+
+#CLASS UI
 class UI:
+    #listas
     times = []
     jogadores = []
 
-    def inserir_time(self):
-        id = int(input("ID do time: "))
-        nome = input("Nome do time: ")
-        estado = input("Estado do time: ")
+    #main (op, while, op = UI.menu())
+    @staticmethod
+    def main():
+        op = -1
+        while op != 0:
+            op = UI.menu()
+            if op == 1: UI.inserir_time()
+            if op == 2: UI.listar_times()
+            if op == 3: UI.atualizar_time()
+            if op == 4: UI.excluir_time()
+            if op == 5: UI.inserir_jogador()
+            if op == 6: UI.listar_jogadores()
+            if op == 7: UI.atualizar_jogador()
+            if op == 8: UI.excluir_jogador()
+            if op == 9: UI.listar_jogadores_time()
+            if op == 10: UI.transferir()
+        print('Programa Encerrado.')
+
+    #menu (print, int(input()))
+    @staticmethod
+    def menu():
+        print('1 - inserir time')
+        print('2 - listar times')
+        print('3 - atualizar time')
+        print('4 - excluir time')
+        print('5 - inserir jogador')
+        print('6 - listar jogadores')
+        print('7 - atualizar jogador')
+        print('8 - excluir jogador')
+        print('9 - listar jogadores de um time')
+        print('10 - transferir jogador')
+        print('0 - Sair')
+        return int(input('Escolha: '))
+
+    #OPERAÇÕES
+    #TIMES
+    @classmethod
+    def inserir_time(cls):
+        id = int(input('ID: '))
+        nome = input('Nome: ')
+        estado = input('Estado: ')
 
         time = Time(id, nome, estado)
-        self.times.append(time)
-        print("Time cadastrado!\n")
+        cls.times.append(time)
 
-    def listar_times(self):
-        if len(self.times) == 0: #ver se a lista está vazia
-            print("Nenhum time cadastrado.\n")
+        print('Time cadastrado!')
+
+    @classmethod
+    def listar_times(cls):
+        if len(cls.times) == 0:
+            print('Não há times cadastrados.')
         else:
-            for time in self.times: #varer a lista
-                print(time)
+            for x in cls.times:
+                print(x)
 
-    def atualizar_time(self):
-        id = int(input("Digite o ID do time: "))
+    @classmethod
+    def atualizar_time(cls):
+        id = int(input('ID do time: '))
 
-        for time in self.times:
-            if time.get_id() == id:
-                novo_nome = input("Novo nome: ")
-                novo_estado = input("Novo estado: ")
+        for x in cls.times:
+            if x.get_id() == id:
+                print('Encontrado!')
 
-                time.set_nome(novo_nome)
-                time.set_estado(novo_estado)
+                novo_nome = input('Novo Nome: ')
+                novo_estado = input('Novo Estado: ')
 
-                print("Time atualizado!\n")
-                return
+                x.set_nome(novo_nome)
+                x.set_estado(novo_estado)
 
-        print("Time não encontrado.\n")
+                print('Atualizado!')
 
-    def excluir_time(self):
-        id = int(input("Digite o ID do time: "))
+    @classmethod
+    def excluir_time(cls):
+        id = int(input('ID do Time: '))
 
-        for time in self.times:
-            if time.get_id() == id:
-                self.times.remove(time)
+        for x in cls.times:
+            if x.get_id() == id:
+                cls.times.remove(x)
 
-                for jogador in self.jogadores[:]:
-                    if jogador.get_id_time() == id:
-                        self.jogadores.remove(jogador)
-
-                print("Time removido!\n")
-                return
-
-        print("Time não encontrado.\n")
-
-    def inserir_jogador(self):
-        id = int(input("ID do jogador: "))
-        id_time = int(input("ID do time: "))
-        nome = input("Nome do jogador: ")
-        camisa = int(input("Número da camisa: "))
-
-        jogador = Jogador(id, id_time, nome, camisa)
-        self.jogadores.append(jogador)
-
-        print("Jogador cadastrado!\n")
-
-    def listar_jogadores(self):
-        if len(self.jogadores) == 0:
-            print("Nenhum jogador cadastrado.\n")
-        else:
-            for jogador in self.jogadores:
-                print(jogador)
-            print()
-
-    def atualizar_jogador(self):
-        id = int(input("Digite o ID do jogador: "))
-
-        for jogador in self.jogadores:
-            if jogador.get_id() == id:
-                novo_nome = input("Novo nome: ")
-                nova_camisa = int(input("Nova camisa: "))
-
-                jogador.set_nome(novo_nome)
-                jogador.set_camisa(nova_camisa)
-
-                print("Jogador atualizado!\n")
-                return
-
-        print("Jogador não encontrado.\n")
-
-    def excluir_jogador(self):
-        id = int(input("Digite o ID do jogador: "))
-
-        for jogador in self.jogadores:
-            if jogador.get_id() == id:
-                self.jogadores.remove(jogador)
-
-                print("Jogador removido!\n")
-                return
-
-        print("Jogador não encontrado.\n")
-
-    def listar_jogadores_do_time(self):
-        id_time = int(input("Digite o ID do time: "))
-
-        encontrou = False
-
-        for jogador in self.jogadores:
-            if jogador.get_id_time() == id_time:
-                print(jogador)
                 encontrou = True
+                print('Removido!')
 
-        if not encontrou:
-            print("Nenhum jogador encontrado nesse time.")
+                for jogador in cls.jogadores:
+                    if jogador.get_idTime() == id:
+                        cls.jogadores.remove(jogador)
 
-        print()
+    #JOGADORES
+    @classmethod
+    def inserir_jogador(cls):
+        id = int(input('ID do Jogador: '))
+        idTime = int(input('ID do Time: '))
+        nome = input('Nome: ')
+        camisa = int(input('Camisa: '))
 
-    def transferir_jogador(self):
-        id_jogador = int(input("ID do jogador: "))
-        novo_time = int(input("Novo ID do time: "))
+        jogador = Jogador(id, idTime, nome, camisa)
+        cls.jogadores.append(jogador)
+        print('Jogador cadastrado!')
 
-        for jogador in self.jogadores:
-            if jogador.get_id() == id_jogador:
-                jogador.set_id_time(novo_time)
+    @classmethod
+    def listar_jogadores(cls):
+        if len(cls.jogadores) == 0:
+            print('Não há jogadores cadastrados.')
+        else:
+            for x in cls.jogadores:
+                print(x)
 
-                print("Jogador transferido!\n")
-                return
+    @classmethod
+    def atualizar_jogador(cls):
+        id = int(input('ID do jogador: '))
 
-        print("Jogador não encontrado.\n")
-    
-    #menu
+        for x in cls.jogadores:
+            if x.get_id() == id:
+                novo_nome = input('Novo Nome: ')
+                nova_camisa = int(input('Nova Camisa: '))
 
-    def main(self):
-        opcao = 1223
+                x.set_nome(novo_nome)
+                x.set_camisa(nova_camisa)
+                print('Atualizado!')
 
-        while opcao != 0:
-            print("1 - Inserir time")
-            print("2 - Listar times")
-            print("3 - Atualizar time")
-            print("4 - Excluir time")
-            print("5 - Inserir jogador")
-            print("6 - Listar jogadores")
-            print("7 - Atualizar jogador")
-            print("8 - Excluir jogador")
-            print("9 - Listar jogadores de um time")
-            print("10 - Transferir jogador")
-            print("0 - Sair")
-            opcao = int(input("Informe uma opção:  "))
+    @classmethod
+    def excluir_jogador(cls):
+        id = int(input('ID do jogador: '))
 
-            if opcao == 1: self.inserir_time()
-            if opcao == 2: self.listar_times()
-            if opcao == 3: self.atualizar_time()
-            if opcao == 4: self.excluir_time()
-            if opcao == 5: self.inserir_jogador()
-            if opcao == 6: self.listar_jogadores()
-            if opcao == 7: self.atualizar_jogador()
-            if opcao == 8: self.excluir_jogador()
-            if opcao == 9: self.listar_jogadores_do_time()
-            if opcao == 10: self.transferir_jogador()
-            if opcao == 0: print("Tchau :)")
+        for x in cls.jogadores:
+            if x.get_id() == id:
+                cls.jogadores.remove(x)
 
-# Programa principal
-ui = UI()
-ui.main()
+                encontrou = True
+                print('Excluído!')
+
+    @classmethod
+    def listar_jogadores_time(cls):
+        id = int(input('ID do Time: '))
+
+        for x in cls.jogadores:
+            if x.get_idTime() == id:
+                print(x)
+
+    @classmethod
+    def transferir(cls):
+        id = int(input('ID do jogador: '))
+
+        for x in cls.jogadores:
+            if x.get_id() == id:
+                print('Encontrei!')
+
+                novo_idTime = int(input('Novo ID do Time: '))
+                x.set_idTime(novo_idTime)
+                print('Transferido!')
+UI.main()
