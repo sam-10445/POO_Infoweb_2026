61a7be8403a62dda2bf72c4c6bf1239f73ab6a69
sam-10445 <smirandanovaes@gmail.com>
2026-06-12 14:58:36 +0000

    Execicio do Treino

--- aula13/Tarefa05_listas_e_datas/ex1_Treino.py
+++ aula13/Tarefa05_listas_e_datas/ex1_Treino.py
@@ -1,73 +1,111 @@
 from datetime import datetime, timedelta
+
 class Treino:
-    #init
+    # init
     def __init__(self, id, data, distancia, tempo):
         self.set_id(id)
         self.set_data(data)
         self.set_distancia(distancia)
         self.set_tempo(tempo)
-    #set
-    def set_id(self, id):
-        if id < 0: raise ValueError()
-        self.__id = id
-    def set_data(self, data):
-        if data > datetime.now(): raise ValueError()
-        self.__data = data
-    def set_distancia(self, distancia):
-        if distancia < 0: raise ValueError()
-        self.__distancia = distancia
-    def set_tempo(self, tempo):
-        if tempo < timedelta(0): raise ValueError()
-        self.__tempo = tempo
-    #get
-    def get_id(self): return self.__id
-    def get_data(self): return self.__data
-    def get_distancia(self): return self.__distancia
-    def get_tempo(self): return self.__tempo
 
+    # set
+    def set_id(self, id):
+        if id < 0:
+            raise ValueError()
+        self.__id = id
+
+    def set_data(self, data):
+        if data > datetime.now():
+            raise ValueError()
+        self.__data = data
+
+    def set_distancia(self, distancia):
+        if distancia <= 0:
+            raise ValueError()
+        self.__distancia = distancia
+
+    def set_tempo(self, tempo):
+        if tempo < timedelta(0):
+            raise ValueError()
+        self.__tempo = tempo
+
+    # get
+    def get_id(self):
+        return self.__id
+
+    def get_data(self):
+        return self.__data
+
+    def get_distancia(self):
+        return self.__distancia
+
+    def get_tempo(self):
+        return self.__tempo
+
+    # pace em minutos/km
     def Pace(self):
-        pace = self.__tempo.total_segunds() / self.__distancia
+        pace = (self.__tempo.total_seconds() / 60) / self.__distancia
         return pace
 
-    #ToString
+    # ToString
     def __str__(self):
-        return f"ID: {self.__id} | Data: {self.__data.strftime("%d/%m/%Y")} | Distância: {self.__distancia} km | Tempo: {self.__tempo} min | Pace: {self.Pace()} min/km"
+        return (
+            f"ID: {self.__id} | "
+            f"Data: {self.__data.strftime('%d/%m/%Y')} | "
+            f"Distância: {self.__distancia} km | "
+            f"Tempo: {self.__tempo} | "
+            f"Pace: {self.Pace():.2f} min/km"
+        )
+
 
 class TreinoUI:
-    #listas
+    # listas
     treinos = []
 
-    #main (op, while, op = TreinoUI.manu())
     @staticmethod
     def main():
         op = -1
+
         while op != 0:
             op = TreinoUI.menu()
-            if op == 1: TreinoUI.inserir()
-            if op == 2: TreinoUI.listar()
-            if op == 3: TreinoUI.listar_id()
-            if op == 4: TreinoUI.atualizar()
-            if op == 5: TreinoUI.excluir()
-            if op == 6: TreinoUI.mais_rapido()
-        print('Programa Encerrado...')
 
-    #menu
+            if op == 1:
+                TreinoUI.inserir()
+
+            if op == 2:
+                TreinoUI.listar()
+
+            if op == 3:
+                TreinoUI.listar_id()
+
+            if op == 4:
+                TreinoUI.atualizar()
+
+            if op == 5:
+                TreinoUI.excluir()
+
+            if op == 6:
+                TreinoUI.mais_rapido()
+
+        print("Programa Encerrado...")
+
     @staticmethod
     def menu():
-        print('1. Inserir')
-        print('2. Listar')
-        print('3. Pesquisar um treino')
-        print('4. Atualizar')
-        print('5. Excluir')
-        print('6. Mais rápido')
-        print('0. Sair')
-        return int(input('Opção: '))
+        print("1. Inserir")
+        print("2. Listar")
+        print("3. Pesquisar um treino")
+        print("4. Atualizar")
+        print("5. Excluir")
+        print("6. Mais rápido")
+        print("0. Sair")
+
+        return int(input("Opção: "))
 
     @classmethod
     def inserir(cls):
-        id = int(input('ID: '))
-        data = datetime.strptime(input('Data (dd/mm/aaaa): '), "%d/%m/%Y")
-        distancia = float(input('Distância (km): '))
+        id = int(input("ID: "))
+        data = datetime.strptime(input("Data (dd/mm/aaaa): "),"%d/%m/%Y")
+        distancia = float(input("Distância (km): "))
         tempo = timedelta(minutes=int(input("Minutos: ")))
 
         x = Treino(id, data, distancia, tempo)
@@ -76,44 +114,62 @@ class TreinoUI:
     @classmethod
     def listar(cls):
         if len(cls.treinos) == 0:
-            print('Não há treinos registrados...')
+            print("Não há treinos registrados...")
         else:
-            for x in cls.treinos: print(x)
+            for x in cls.treinos:
+                print(x)
 
     @classmethod
     def listar_id(cls):
-        id = int(input('ID do treino: '))
+        id = int(input("ID do treino: "))
+
         for x in cls.treinos:
-            if x.get_id() == id: print(x)
+            if x.get_id() == id:
+                print(x)
+                return
+
+        print("Treino não encontrado.")
 
     @classmethod
     def atualizar(cls):
-        id = int(input('ID do treino: '))
+        id = int(input("ID do treino: "))
+
         for x in cls.treinos:
             if x.get_id() == id:
-                data = datetime.strptime(input('Data (dd/mm/aaaa): '), "%d/%m/%Y")
-                distancia = float(input('Distância (m): '))
+                data = datetime.strptime(input("Data (dd/mm/aaaa): "),"%d/%m/%Y")
+                distancia = float(input("Distância (km): "))
                 tempo = timedelta(minutes=int(input("Minutos: ")))
 
                 x.set_data(data)
                 x.set_distancia(distancia)
-                x.get_tempo(tempo)
+                x.set_tempo(tempo)
+
+                print("Treino atualizado.")
+                return
+
+        print("Treino não encontrado.")
 
     @classmethod
     def excluir(cls):
-        id = int(input('Id do Treino: '))
+        id = int(input("ID do treino: "))
+
         for x in cls.treinos:
             if x.get_id() == id:
                 cls.treinos.remove(x)
+                print("Treino removido.")
+                return
+
+        print("Treino não encontrado.")
 
     @classmethod
     def mais_rapido(cls):
-        paces = []
         if len(cls.treinos) == 0:
-            print('Não há treinos registrados...')
+            print("Não há treinos registrados...")
         else:
-            for x in cls.treinos:
-                paces += x.Pace()
-                min(paces)
+            rapido = min(cls.treinos, key=lambda x: x.Pace())
+
+            print("\nTreino mais rápido:")
+            print(rapido)
+
 
 TreinoUI.main()
