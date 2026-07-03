8aff6c2d745d701cab78feb2869e68f7eae42a0f
sam-10445 <smirandanovaes@gmail.com>
2026-07-03 13:03:12 +0000

    tentativa de fazer a lista de POO 07 - Programação em Camadas

---
+++ aula19/ListaPoo07/models/service.py
@@ -0,0 +1,63 @@
+from models.cliente import Cliente
+from models.clientedao import ClienteDAO
+
+from models.servico import Servico
+from models.servicodao import ServicoDAO
+
+
+class Service:
+
+    __clienteDAO = ClienteDAO()
+    __servicoDAO = ServicoDAO()
+
+    # =====================================
+    # CLIENTES
+    # =====================================
+
+    @staticmethod
+    def cliente_inserir(id, nome, email, fone):
+        obj = Cliente(id, nome, email, fone)
+        Service.__clienteDAO.inserir(obj)
+
+    @staticmethod
+    def cliente_listar():
+        return Service.__clienteDAO.listar()
+
+    @staticmethod
+    def cliente_listar_id(id):
+        return Service.__clienteDAO.listar_id(id)
+
+    @staticmethod
+    def cliente_atualizar(id, nome, email, fone):
+        obj = Cliente(id, nome, email, fone)
+        Service.__clienteDAO.atualizar(obj)
+
+    @staticmethod
+    def cliente_excluir(id):
+        Service.__clienteDAO.excluir(id)
+
+    # =====================================
+    # SERVIÇOS
+    # =====================================
+
+    @staticmethod
+    def servico_inserir(id, descricao, valor):
+        obj = Servico(id, descricao, valor)
+        Service.__servicoDAO.inserir(obj)
+
+    @staticmethod
+    def servico_listar():
+        return Service.__servicoDAO.listar()
+
+    @staticmethod
+    def servico_listar_id(id):
+        return Service.__servicoDAO.listar_id(id)
+
+    @staticmethod
+    def servico_atualizar(id, descricao, valor):
+        obj = Servico(id, descricao, valor)
+        Service.__servicoDAO.atualizar(obj)
+
+    @staticmethod
+    def servico_excluir(id):
+        Service.__servicoDAO.excluir(id)
